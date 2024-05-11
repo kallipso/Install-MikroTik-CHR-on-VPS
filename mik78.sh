ec80ae03d5f93f3401a3bded7cac73b3a01bfe7f
kallipso <kallipso.v@gmail.com>
2024-05-11 10:51:08 +0200

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -21,3 +21,5 @@ dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
 echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
 echo b > /proc/sysrq-trigger && \
+echo "Ok, reboot" && \
+echo b > /proc/sysrq-trigger
