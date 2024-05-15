03d39d4958b219e5d1517129adf9ffefc79123fe
kallipso <kallipso.v@gmail.com>
2024-05-15 20:04:49 +0200

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -8,6 +8,7 @@ echo
 sleep 3
 wget https://download.mikrotik.com/routeros/7.5/chr-7.5.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=512 chr.img /mnt && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
@@ -16,10 +17,18 @@ ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
 echo ADDRESS is $ADDRESS && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo GATEWAY is $GATEWAY && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/user set 0 name=root password=root
+/interface ethernet reset-mac-address numbers=0
+ " > /mnt/run.auto.rsc && \
+umount /mnt && \
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=/dev/$STORAGE && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Sleep 5 seconds" && \
 sleep 5 && \
-dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
-echo "Ok, reboot" && \
-echo 1 > /proc/sys/kernel/sysrq && \
-echo b > /proc/sysrq-trigger && \
 echo "Ok, reboot" && \
 echo b > /proc/sysrq-trigger
